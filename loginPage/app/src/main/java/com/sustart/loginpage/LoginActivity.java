cf323ddfb7b8863b78495c6d1e92010664a3548f
MrGo123 <zymail68@foxmail.com>
2021-12-02 21:58:39 +0800

    update

---
+++ loginPage/app/src/main/java/com/sustart/loginpage/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.sustart.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Boolean bPwdSwitch = false;
+    private EditText etPwd;
+    private EditText etAccount;
+    private CheckBox cbRememberPwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        实现点击选择是否密码可见的功能
+        final ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+        etPwd = findViewById(R.id.et_pwd);
+
+        ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bPwdSwitch = !bPwdSwitch;
+                if (bPwdSwitch) {
+                    ivPwdSwitch.setImageResource(R.drawable.ic_baseline_visibility_24);
+                    etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    ivPwdSwitch.setImageResource(R.drawable.ic_baseline_visibility_off_24);
+                    etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    etPwd.setTypeface(Typeface.DEFAULT);
+                }
+            }
+        });
+
+//      实现记住密码的功能：remember password
+        etPwd = findViewById(R.id.et_pwd);
+        etAccount = findViewById(R.id.etAccount);
+        cbRememberPwd = findViewById(R.id.cb_remember_pwd);
+        Button btLogin = findViewById(R.id.bt_Login);
+        btLogin.setOnClickListener(this);
+
+        String spFileName = getResources().getString(R.string.shared_preference_file_name);
+        String accountKey = getResources().getString(R.string.login_account_name);
+        String passwordKey = getResources().getString(R.string.login_password);
+        String rememberPasswordKey = getResources().getString(R.string.login_remember_password);
+        SharedPreferences spFile = getSharedPreferences(spFileName, Context.MODE_PRIVATE);
+        String account = spFile.getString(accountKey, null);
+        String password = spFile.getString(passwordKey, null);
+        Boolean rememberPassword = spFile.getBoolean(rememberPasswordKey, false);
+
+        if (account != null && !TextUtils.isEmpty(account)) {
+            etAccount.setText(account);
+        }
+        if (password != null && !TextUtils.isEmpty(password)) {
+            etPwd.setText(password);
+        }
+        cbRememberPwd.setChecked(rememberPassword);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String spFileName = getResources().getString(R.string.shared_preference_file_name);
+        String accountKey = getResources().getString(R.string.login_account_name);
+        String passwordKey = getResources().getString(R.string.login_password);
+        String rememberPasswordKey = getResources().getString(R.string.login_remember_password);
+
+        SharedPreferences spFile = getSharedPreferences(spFileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = spFile.edit();
+
+        if (cbRememberPwd.isChecked()) {
+            String password = etPwd.getText().toString();
+            String account = etAccount.getText().toString();
+
+            editor.putString(accountKey, account);
+            editor.putString(passwordKey, password);
+            editor.putBoolean(rememberPasswordKey, true);
+            editor.apply();
+        } else {
+            editor.remove(accountKey);
+            editor.remove(passwordKey);
+            editor.remove(rememberPasswordKey);
+            editor.apply();
+        }
+
+    }
+}
--- newsList/app/src/main/java/com/sustart/newslist/DetailsActivity.java
+++ newsList/app/src/main/java/com/sustart/newslist/DetailsActivity.java
@@ -4,19 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
     private String[] titles = null;
     private String[] authors = null;
-// 使用ArrayAdapter类
+    private static final String NEWS_TITLE = "news_title";
+    private static final String NEWS_AUTHOR = "news_author";
+    private List<Map<String, String>> dataList = new ArrayList<>();
+    public List<News> newsList = new ArrayList<>();
+
+//    从xml文件中读取数据资源
+    private void initData() {
+        int length;
+        titles = getResources().getStringArray(R.array.titles);
+        authors = getResources().getStringArray(R.array.authors);
+        TypedArray images = getResources().obtainTypedArray(R.array.images);
+        if (titles.length > authors.length) {
+            length = authors.length;
+        } else {
+            length = titles.length;
+        }
+
+        for (int i = 0; i < length; i++) {
+            News news = new News();
+            news.setmTitle(titles[i]);
+            news.setmAuthor(titles[i]);
+            news.setmImageId(images.getResourceId(i, 0));
+            newsList.add(news);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        initData();
+
+//        使用自定义的适配器
+        NewsAdapter newsAdapter = new NewsAdapter(DetailsActivity.this, R.layout.list_item, newsList);
+
+        ListView lvNewsList = findViewById(R.id.lv_news_list);
+        lvNewsList.setAdapter(newsAdapter);
+    }
+
+    // 使用ArrayAdapter类
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
@@ -31,10 +68,6 @@ public class MainActivity extends AppCompatActivity {
 //
 //    }
 
-    private static final String NEWS_TITLE = "news_title";
-    private static final String NEWS_AUTHOR = "news_author";
-    private List<Map<String, String>> dataList = new ArrayList<>();
-    public List<News> newsList = new ArrayList<>();
 
 //    使用Map实现标题和作者时
 //    private void initData() {
@@ -72,38 +105,4 @@ public class MainActivity extends AppCompatActivity {
 //        ListView lvNewsList = findViewById(R.id.lv_news_list);
 //        lvNewsList.setAdapter(simpleAdapter);
 //    }
-//    从xml文件中读取数据资源
-    private void initData() {
-        int length;
-        titles = getResources().getStringArray(R.array.titles);
-        authors = getResources().getStringArray(R.array.authors);
-        TypedArray images = getResources().obtainTypedArray(R.array.images);
-        if (titles.length > authors.length) {
-            length = authors.length;
-        } else {
-            length = titles.length;
-        }
-
-        for (int i = 0; i < length; i++) {
-            News news = new News();
-            news.setmTitle(titles[i]);
-            news.setmAuthor(titles[i]);
-            news.setmImageId(images.getResourceId(i, 0));
-            newsList.add(news);
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initData();
-
-//        使用自定义的适配器
-        NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, R.layout.list_item, newsList);
-
-        ListView lvNewsList = findViewById(R.id.lv_news_list);
-        lvNewsList.setAdapter(newsAdapter);
-    }
 }
