ea7f50c40fed062694f57901e67d39965b83d79d
MrGo123 <zymail68@foxmail.com>
2021-12-23 21:23:07 +0800

    update

--- SHDSystem/app/src/main/java/com/sustart/shdsystem/LoginActivity.java
+++ SHDSystem/app/src/main/java/com/sustart/shdsystem/LoginActivity.java
@@ -10,11 +10,26 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     private Button registerButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //        注册按钮绑定单击事件
+        loginButton = findViewById(R.id.login_btn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                用户信息判断逻辑
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 //        注册按钮绑定单击事件
         registerButton = findViewById(R.id.register_btn);
--- SHDSystem/app/src/main/java/com/sustart/shdsystem/RegisterActivity.java
+++ SHDSystem/app/src/main/java/com/sustart/shdsystem/RegisterActivity.java
@@ -8,22 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Button backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        // 返回按钮绑定单击事件
-        backButton = findViewById(R.id.back_btn);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
